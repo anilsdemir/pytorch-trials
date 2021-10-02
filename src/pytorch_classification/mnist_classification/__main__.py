002407a87b580e19ae25d8888bbef0379b8c6453
Anil Demir <anilsdemir@gmail.com>
2021-10-02 20:51:49 +0300

    PT-2-mnist-classification logging ability is added gpu device

--- src/pytorch_classification/mnist_classification/__main__.py
+++ src/pytorch_classification/mnist_classification/__main__.py
@@ -6,9 +6,12 @@ import torch
 import numpy as np
 
 from pytorch_classification.utils.gpu import (
-    get_gpu_info
+    get_gpu_info,
+    create_gpu_device
 )
 
 
 if __name__ == '__main__':
+    device = create_gpu_device()
     get_gpu_info()
+
--- src/pytorch_classification/utils/gpu.py
+++ src/pytorch_classification/utils/gpu.py
@@ -19,7 +19,23 @@ def create_gpu_device():
             f"No available cudas were found at this time."
             f"Please close programs that use cuda."
         )
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    logging.warning("CUDA device created")
+
+    if device.type == 'cuda':
+        logging.warning(
+            '<--- Memory Usage --->'
+        )
+        logging.warning(
+            'Allocated: '
+            f'{str(round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1))} GB'
+        )
+        logging.warning(
+            'Cached: '
+            f'{str(round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1))} GB'
+        )
+
     return device
 
 
@@ -29,9 +45,10 @@ def get_gpu_info():
             f"No available cudas were found at this time."
             f"Please close programs that use cuda."
         )
-    print(
+    logging.info(
+        '"""""""""""""""""""""""""""""\n'
         f"Device Name: {torch.cuda.get_device_name(0)}\n"
         f"Device Count: {torch.cuda.device_count()}\n"
-        f"Device Capability: {torch.cuda.get_device_capability()}"
+        f"Device Capability: {torch.cuda.get_device_capability()}\n"
+        '"""""""""""""""""""""""""""""'
     )
-
